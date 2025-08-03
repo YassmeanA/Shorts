db1bd4c23edf1c3e045e17663630d7ad48ae7b28
YassmeanA <yassmeanahmed23@gmail.com>
2025-08-03 18:28:55 +0200

    Update Shorts.js

--- Shorts.js
+++ Shorts.js
@@ -80,7 +80,7 @@ let countB;
 let currentIndex = 0;
 let currentVideoIndex = null;
 
-setTimeout(() => {Pages[0].style.opacity = '1';BottomNavBar.style.opacity = '1'},1000);
+setTimeout(() => {Pages[0].style.opacity = '1';BottomNavBar.style.opacity = '1';},1000);
 
 //Remove loading animation after the page is fully loaded
 Pages[0].addEventListener('load', () => {
@@ -1396,6 +1396,7 @@ Save.style.display = "none"; // hide again
     }, 1500);
   }, 600);
 });
+
 
 
 
